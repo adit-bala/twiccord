cf16f201eee3c38384b97849fe64464692ca4d31
adit-bala <adityarbala@gmail.com>
2022-10-22 09:05:58 -0700

    format tweets within embeds (roughly)

--- bot.py
+++ bot.py
@@ -31,8 +31,13 @@ async def on_ready():
 
 @bot.command()
 async def tweets(ctx):
-    my_tweets = client.get_users_tweets(id=os.getenv('USER_ID'), user_auth=True).data
-    await ctx.send(str(my_tweets))
+    my_tweets = client.get_users_tweets(id=os.getenv('USER_ID'), user_auth=True, tweet_fields="created_at")
+    embed=discord.Embed(title="Recent Tweets", description="Recent Tweets from @existentialraj", color=0xe8d1e7)
+    embed.set_author(name=ctx.author.display_name, url="https://twitter.com/existentialraj", icon_url="https://i.imgur.com/riyrHKH.jpg")
+    for tweet in my_tweets.data:
+        embed.add_field(name=tweet.created_at, value=tweet.text)
+        
+    await ctx.send(embed=embed)
 
 
 bot.run(os.getenv('DISCORD_BOT'))
