a0006a0bbc23ea5a6e9fc58c2aac3348709d2c09
adit-bala <adityarbala@gmail.com>
2022-10-22 21:08:35 -0700

    format dates properly within embeds
    - I wish to never interact with a `datetime` object again

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
 import os
 import tweepy
 import discord
+import calendar
 from discord.ext import commands
-import random
 from dotenv import load_dotenv
+from datetime import datetime, timezone 
+
+
+
+
 load_dotenv()
-
-
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
@@ -19,7 +22,6 @@ client = tweepy.Client(
 )
 
 
-
 bot = commands.Bot(command_prefix='?', intents=intents)
 
 
@@ -30,15 +32,22 @@ async def on_ready():
 
 
 @bot.command()
-async def tweets(ctx):
-    my_tweets = client.get_users_tweets(id=os.getenv('USER_ID'), user_auth=True, tweet_fields="created_at")
-    embed=discord.Embed(title="Recent Tweets", description="Recent Tweets from @existentialraj", color=0xe8d1e7)
-    embed.set_author(name=ctx.author.display_name, url="https://twitter.com/existentialraj", icon_url="https://i.imgur.com/riyrHKH.jpg")
-    for tweet in my_tweets.data:
-        embed.add_field(name=tweet.created_at, value=tweet.text)
-        
+async def tweets(ctx, arg=5):
+    my_tweets = client.get_users_tweets(id=os.getenv(
+        'USER_ID'), user_auth=True, tweet_fields="created_at")
+    embed = discord.Embed(
+        title="ruminations from the mind of adit", color=0xe8d1e7)
+    embed.set_author(name=ctx.author.display_name, url="https://twitter.com/existentialraj",
+                     icon_url=ctx.author.avatar)
+    for i in range(arg):
+        tweet = my_tweets.data[i]
+        date = tweet.created_at.replace(tzinfo=timezone.utc).astimezone(tz=None).date()
+        time = tweet.created_at.replace(tzinfo=timezone.utc).astimezone(tz=None).time()
+        embed.add_field(
+            name=f"{calendar.month_name[date.month]} {date.day}, {date.year} at {datetime.strptime(str(time)[:-3],'%H:%M').strftime('%I:%M %p')}", value=tweet.text, inline=False)
+    embed.set_footer(
+        text=f"{ctx.author.display_name} is too lazy to download twitter")
     await ctx.send(embed=embed)
 
 
 bot.run(os.getenv('DISCORD_BOT'))
-
