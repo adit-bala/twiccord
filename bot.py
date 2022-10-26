cda76ca0adad2ea78bf611314505243a9af10f1e
adit-bala <adityarbala@gmail.com>
2022-10-26 09:27:44 -0700

    create `?today` command

--- bot.py
+++ bot.py
@@ -4,9 +4,7 @@ import discord
 import calendar
 from discord.ext import commands
 from dotenv import load_dotenv
-from datetime import datetime, timezone 
-
-
+from datetime import datetime, timezone
 
 
 load_dotenv()
@@ -41,20 +39,23 @@ async def num(ctx, arg=5):
                      icon_url=ctx.author.avatar)
     for i in range(arg if arg < 100 else 50):
         tweet = my_tweets.data[i]
-        date = tweet.created_at.replace(tzinfo=timezone.utc).astimezone(tz=None).date()
-        time = tweet.created_at.replace(tzinfo=timezone.utc).astimezone(tz=None).time()
+        date = tweet.created_at.replace(
+            tzinfo=timezone.utc).astimezone(tz=None).date()
+        time = tweet.created_at.replace(
+            tzinfo=timezone.utc).astimezone(tz=None).time()
         embed.add_field(
             name=f"{calendar.month_name[date.month]} {date.day}, {date.year} at {datetime.strptime(str(time)[:-3],'%H:%M').strftime('%I:%M %p')}", value=tweet.text, inline=False)
     embed.set_footer(
         text=f"{ctx.author.display_name} is too lazy to download twitter")
     await ctx.send(embed=embed)
 
+
 @bot.command()
 async def today(ctx):
-    today = datetime.now(timezone.utc).astimezone().isoformat()
+    today = datetime.today().isoformat()[:-7] + "Z"
     my_tweets = client.get_users_tweets(id=os.getenv(
-        'USER_ID'), user_auth=True, tweet_fields="created_at", exclude="retweets", end_time=today)
-    if not len(my_tweets.data):
+        'USER_ID'), user_auth=True, tweet_fields="created_at", exclude="retweets", start_time=today)
+    if not my_tweets.data:
         await ctx.send("No tweets yet today :(")
         return
     embed = discord.Embed(
@@ -62,8 +63,10 @@ async def today(ctx):
     embed.set_author(name=ctx.author.display_name, url="https://twitter.com/existentialraj",
                      icon_url=ctx.author.avatar)
     for tweet in my_tweets.data:
-        date = tweet.created_at.replace(tzinfo=timezone.utc).astimezone(tz=None).date()
-        time = tweet.created_at.replace(tzinfo=timezone.utc).astimezone(tz=None).time()
+        date = tweet.created_at.replace(
+            tzinfo=timezone.utc).astimezone(tz=None).date()
+        time = tweet.created_at.replace(
+            tzinfo=timezone.utc).astimezone(tz=None).time()
         embed.add_field(
             name=f"{calendar.month_name[date.month]} {date.day}, {date.year} at {datetime.strptime(str(time)[:-3],'%H:%M').strftime('%I:%M %p')}", value=tweet.text, inline=False)
     embed.set_footer(
