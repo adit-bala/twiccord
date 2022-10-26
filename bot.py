a9508598b5d953caeea06465abb765787ce7a224
adit-bala <adityarbala@gmail.com>
2022-10-26 16:50:22 -0700

    refactor iteration of tweets

--- bot.py
+++ bot.py
@@ -33,11 +33,13 @@ async def on_ready():
 async def num(ctx, arg=5):
     my_tweets = client.get_users_tweets(id=os.getenv(
         'USER_ID'), user_auth=True, tweet_fields="created_at", max_results=100, exclude="retweets")
+    tweets_range = arg if 0 < arg < 100 else 50
+    delta = datetime.today().date() - my_tweets.data[tweets_range-1].created_at.date()
     embed = discord.Embed(
-        title="ruminations from the mind of adit", color=0xe8d1e7)
+        title=f"ruminations from the past {delta.days} days", color=0xe8d1e7)
     embed.set_author(name=ctx.author.display_name, url="https://twitter.com/existentialraj",
                      icon_url=ctx.author.avatar)
-    for i in range(arg if arg < 100 else 50):
+    for i in range(tweets_range):
         tweet = my_tweets.data[i]
         date = tweet.created_at.replace(
             tzinfo=timezone.utc).astimezone(tz=None).date()
@@ -60,17 +62,11 @@ async def today(ctx):
         return
     embed = discord.Embed(
         title="today's thoughts", color=0xe8d1e7)
-    embed.set_author(name=ctx.author.display_name, url="https://twitter.com/existentialraj",
-                     icon_url=ctx.author.avatar)
     for tweet in my_tweets.data:
-        date = tweet.created_at.replace(
-            tzinfo=timezone.utc).astimezone(tz=None).date()
         time = tweet.created_at.replace(
             tzinfo=timezone.utc).astimezone(tz=None).time()
         embed.add_field(
-            name=f"{calendar.month_name[date.month]} {date.day}, {date.year} at {datetime.strptime(str(time)[:-3],'%H:%M').strftime('%I:%M %p')}", value=tweet.text, inline=False)
-    embed.set_footer(
-        text=f"{ctx.author.display_name} is too lazy to download twitter")
+            name=f"@ {datetime.strptime(str(time)[:-3],'%H:%M').strftime('%I:%M %p')}", value=tweet.text, inline=False)
     await ctx.send(embed=embed)
 
 
